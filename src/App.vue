29cd72b0a01e849b0c8312e40472c9ac85fe0b1f
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2022-12-07 13:48:52 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,40 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <NavBar class="nav-bar"/>
+    <div class="main-container">
+        <router-view class="main-view"/>
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
+.nav-bar {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+    justify-content: space-between;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 30px;
+    width: 100%;
+    border-bottom: 1px solid var(--color-border);
+}
+.main-container {
+    position: absolute;
+    top: 30px;
+    bottom: 0;
+    width: 100%;
+    padding-left: 0.3em;
+    padding-right: 0.3em;
+    overflow: hidden;
+}
+.main-view {
+    height: 100%;
+    width: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+    margin-top: 0.3em;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <nav>
+        <div>
+            <router-link :to="{ name: 'home' }" class="home-page-link">
+                Ringo
+            </router-link>
+        </div>
+        <div class="nav-bar-menu">
+            <div>
+                Temporário
+            </div>
+            <div>
+                Temporário
+            </div>
+            <div>
+                Temporário
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.nav-bar-menu {
+    margin-right: 20px;
+    display: flex;
+}
+.nav-bar-menu div {
+    align-self: center;
+    margin-left: 10px;
+}
+.home-page-link {
+    font-size: 1.2em;
+    font-weight: bolder;
+    padding-left: 10px;
+}
+</style>
