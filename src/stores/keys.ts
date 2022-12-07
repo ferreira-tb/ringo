e5eac82c923541843c287c447d70f0fe02827b98
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2022-12-07 20:12:34 -0300

    delete keys.ts

--- src/stores/keys.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia';
-import { readonly, ref } from 'vue';
-
-export const useKeysStore = defineStore('keys', () => {
-    const createCharKey = ref(Symbol());
-    const createChar = readonly(createCharKey);
-
-    return { createChar };
-});
