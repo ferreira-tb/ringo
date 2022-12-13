7675401229f247c5f7cdcf226e7dfd42302c929b
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2022-12-13 18:59:42 -0300

    update

--- assets/Button.c941bf8a.js
+++
@@ -1,1 +0,0 @@
-import{d as o,h as a,o as s,c,t as _,n as p,_ as l}from"./index.ed7861fd.js";const r=o({__name:"Button",props:{text:null,padding:null},setup(e){const t=e,n=a({padding:t.padding||"0.3em"});return(d,u)=>(s(),c("button",{class:"standard-button",style:p(n)},_(t.text),5))}});const m=l(r,[["__scopeId","data-v-cf0e5a9c"]]);export{m as B};
