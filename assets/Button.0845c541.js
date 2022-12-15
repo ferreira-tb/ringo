e78bcc09fd47b3c85b98482dbcd1a288696c1536
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2022-12-15 19:50:41 -0300

    update

---
+++ assets/Button.0845c541.js
@@ -0,0 +1,1 @@
+import{d as e,o,c as n,t as s,_}from"./index.0b507058.js";const a={class:"standard-button"},c=e({__name:"Button",props:{text:null},setup(t){return(p,r)=>(o(),n("button",a,s(t.text),1))}});const u=_(c,[["__scopeId","data-v-a0d15b53"]]);export{u as B};
