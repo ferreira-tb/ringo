977d447497a19cdc45fc2c1825a195d88bdd77fa
ferreira-tb <109777783+ferreira-tb@users.noreply.github.com>
2023-01-09 13:57:23 -0300

    update helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -13,20 +13,6 @@ export function generateDiceRollText(dice: number, amount: number, modToSum: num
     return rollText;
 };
 
-/** Retorna informações sobre as habilidades do jogo. */
-export async function fetchAbilities(): Promise<ReadonlyMap<Abilities, APIAbilityInfo>> {
-    const response = await fetch('/ringo/api/ability.json');
-    const jsonArray = await response.json() as APIAbilityInfo[];
-    
-    if (!Array.isArray(jsonArray)) {
-        throw new RingoError('Não foi possível obter informações sobre as habilidades.');
-    };
-
-    const abilityMap = new Map<Abilities, APIAbilityInfo>();
-    jsonArray.forEach((item) => abilityMap.set(item.sigla, item));
-    return abilityMap;
-};
-
 /**
  * Emite um arquivo JSON para download.
  * @param item Item que será convertido para emissão.
